c631f694bfd4a7f06d2dcb07631450c78b422aa1
fluffytoycoy <40921499+fluffytoycoy@users.noreply.github.com>
2018-07-10 21:52:36 -0500

    New methods
    Added set/getClosed for future implementations. comparableDate1&2() for checking the date against lpn date.
    Overall better optimized the var date usage.

--- A_Scanner/src/com/dockDoors/DockDoors.java
+++ A_Scanner/src/com/dockDoors/DockDoors.java
@@ -1,36 +1,47 @@
 package com.dockDoors;
 
-public class DockDoors {
-
-	private String date1;
-	private String date2;
+public class DockDoor {
+	//Eventually DockDoor and Locator will extend class Location. Is-a
+	private int date1;
+	private int date2;
 	private String department;
-
-	public DockDoors() {
+	private boolean closed;
+	
+	public DockDoor() {
 		super();
 	}
 
-	public DockDoors(String department, String date1, String date2) {
+	public DockDoor(String department, String date1, String date2) {
 		this.department = department;
-		this.date1 = date1;
-		this.date2 = date2;
+		try {
+		this.date1 = Integer.parseInt(date1);
+		}catch(NumberFormatException e) {
+			System.out.println("String made it to the date");	}
+		try {
+			this.date2 = Integer.parseInt(date2);
+			}catch(NumberFormatException e) {
+				System.out.println("String made it to the date");	
+			}
+		closed = false;
 
 	}
-
-	public DockDoors(String department) {
+	
+	public DockDoor(String department) {
 		this.department = department;
 	}
 
 	public boolean setDate1(String date1) {
-		// boolean for saying the date is good to exit loop.
-		// input loaded up
-		boolean loaded = false;
+		// boolean return for saying the date is good to outside exit loop.
+		// takes String input changes to int for date storage.
+		// could change to int but thinking of implementing string identifiers for months.
+		
+		boolean dateLoaded = false;
 		try {
 			if (date1.length() == 8) {
 				if (!(Integer.parseInt(date1.substring(0, 2)) > 12)) {
 					if (!(Integer.parseInt(date1.substring(2, 4)) > 30)) {
-						this.date1 = date1;
-						loaded = true;
+						this.date1 = Integer.parseInt(date1);
+						dateLoaded = true;
 					} else {
 						System.out.println("incorrect MONTH format");
 					}
@@ -46,30 +57,32 @@ public class DockDoors {
 			//Try catch with exception for characters instead of int.
 			
 		}
-		if (loaded) {
+		if (dateLoaded) {
 			System.out.println("date set to: " + date1.substring(0, 2) + "/" + date1.substring(2, 4) + "/"
 					+ date1.substring(4, 8));
 			
 		}
-		return !loaded;
+		return !dateLoaded;
+		//
+		// While date is not within 12/30/XXXX repeat.
+		//
+		// print date set MM/DD/YYYY
+		// done by external method.
 	}
 	
-
-	//
-	// While date is not within 12/30/XXXX repeat.
-	//
-	// print date set MM/DD/YYYY
-
 	public boolean setDate2(String date2) {
-		// boolean for saying the date is good to exit loop.
-		// input loaded up
+		// boolean return for saying the date is good to outside exit loop.
+		// takes String input changes to int for date storage.
+		// could change to int but thinking of implementing string identifiers for months.
+		// Unlike setDate1 Date2 allows empty input to put for no bound on tbd datecheck().
+		
 		boolean loaded = false;
-		if (!(date2.isEmpty())) { 
+		if (!(date2.isEmpty())|| (Integer.parseInt(date2) == 0)) { 
 		try {
 			if (date2.length() == 8) {
 				if (!(Integer.parseInt(date2.substring(0, 2)) > 12)) {
 					if (!(Integer.parseInt(date2.substring(2, 4)) > 30)) {
-						this.date2 = date2;
+						this.date2 = Integer.parseInt(date2);
 						loaded = true;
 					} else {
 						System.out.println("incorrect MONTH format");
@@ -88,31 +101,93 @@ public class DockDoors {
 		}
 		if (loaded) {
 			System.out.println("date set to: " + date2.substring(0, 2) + "/" + date2.substring(2, 4) + "/"
-					+ date1.substring(4, 8));
+					+ date2.substring(4, 8));
 		}
 	} else {
-		this.date2 = null;
+		this.date2 = -1;
 	}
 		return loaded;
 	
 }	
-	
-	public String getDepartment() {
-		return department;
-	}
 
 	public void setDepartment(String department) {
 		this.department = department;
 	}
 
+	public int comparableDate1() {
+		//Normal date is MM/DD/YYYY not eaisly comparable.
+		//This method flips the date around to YYYY/MM/DD for easy comparisons.
+		
+		int d;
+		if(this.date1 > 0) {
+		String tempDate = Integer.toString(this.date1);
+		String stringDate ="";
+		stringDate = tempDate.substring(4,8) + tempDate.substring(0,2) + tempDate.substring(2,4);
+		return Integer.parseInt(stringDate);
+		}else {
+			d = 0;
+		}
+		return d;
+	}
+
 	public String getDate1() {
-		return date1;
+		//returns a readable date format for screen display
+		//returns "No date Selected" if date = 0;
+		
+		String d;
+		if (this.date1 > 0) {
+		d = Integer.toString(this.date1);
+		d = d.substring(0, 2) + "/" + d.substring(2, 4) + "/"
+		+ d.substring(4, 8);
+		} else {
+			d = "No date Selected";
+		}
+		return d;
 	}
-
+	
 	public String getDate2() {
-		return date2;
+		//returns a readable date format for screen display
+		//returns "No date Selected" if date = 0;
+		
+		String d;
+		if (this.date2 > 0) {
+		d = Integer.toString(this.date2);
+		d = d.substring(0, 2) + "/" + d.substring(2, 4) + "/"
+		+ d.substring(4, 8);
+		} else {
+			d = "No date Selected";
+		}
+		return d;
+	}
+	
+	public int comparableDate2() {
+		//Normal date is MM/DD/YYYY not eaisly comparable.
+		//This method flips the date around to YYYY/MM/DD for easy comparisons.
+
+		int d;
+		if(this.date2 > 0) {
+		String tempDate = Integer.toString(this.date2);
+		String stringDate ="";
+		stringDate = tempDate.substring(4,8) + tempDate.substring(0,2) + tempDate.substring(2,4);
+		d = Integer.parseInt(stringDate);
+		}else {
+			d = 0;
+		}
+		return d;
 	}
 
-	// SimpleDateFormat date1a = new SimpleDateFormat();
+	public boolean getClosed() {
+		return closed;
+	}
+	
+	public String getDepartment() {
+		return department;
+	}
 
+	public void setClosed(boolean closed) {
+		this.closed = closed;
+		//this method will eventually be called by closeOut();
+		//closeOut() will change all lpns in dockdoor list to unmovable (boolean) and make getters and setters not work. UNLOAD IS THE ONLY WAY TO STOP THIS.
+	}
 }
+
